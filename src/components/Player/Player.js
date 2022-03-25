1a7e12f5a68bf65640b2157400bc27e5a193a6f6
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-03-25 16:48:20 +0600

    added modal

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import './Player.css';
 
 const Player = ({ player }) => {
   const { img, name, Salary } = player;
   return (
-    <div className="col-md-4">
-      <Card style={{ width: '18rem' }}>
+    <div className="col-md-4 g-3 player">
+      <Card>
         <Card.Img variant="top" src={img} />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Text>{Salary} USD</Card.Text>
-          <Button variant="primary">Hire</Button>
+          <Button variant="">Hire Player</Button>
         </Card.Body>
       </Card>
     </div>
--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,26 +1,63 @@
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
 import Player from '../Player/Player';
+import './Players.css';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
 
 const Players = () => {
   const [players, setPlayers] = useState([]);
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   useEffect(() => {
     fetch(
       'https://raw.githubusercontent.com/SyedMonir/bpl-auction/main/public/players.json'
     )
+      // fetch('players.json')
       .then((res) => res.json())
       .then((data) => setPlayers(data));
   }, []);
-  console.log(players);
+  //   console.log(players);
 
   return (
-    <div className="container">
-      <div className="row">
-        {players.map((player) => (
-          <Player key={player.id} player={player}></Player>
-        ))}
+    <section id="players">
+      <div className="container">
+        <div className="row">
+          {players.map((player) => (
+            <Player key={player.id} player={player}></Player>
+          ))}
+        </div>
       </div>
-    </div>
+      <div id="cart" onClick={openModal}>
+        🛒 <sup>0</sup>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      ></Modal>
+    </section>
   );
 };
 
